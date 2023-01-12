6084e724b5e2b57e637c52cb7dea6e002f3a0b65
Santiago <santiago.martinez@eurofirms.com>
2023-01-13 00:12:03 +0100

    Clean files Install eslint Add minor example

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "standard"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "space-before-function-paren": "off",
+    quotes: "off",
+    "comma-dangle": "off",
+    semi: "off",
+    "react/prop-types": "off",
+  },
+};
