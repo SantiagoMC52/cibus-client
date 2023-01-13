1f6d273ef4d01e3623d09960272aa54e2da2db70
Santiago <santiago.martinez@eurofirms.com>
2023-01-13 22:32:01 +0100

    Install react router dom and create minimal pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,42 @@
-import { useEffect, useState } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Link,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signin from "./components/Signin";
 
-type Post = {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
-}[];
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Link to="/">HOME</Link>
+        <Link to="/login">LOGIN</Link>
+        <Link to="/signin">SIGIN</Link>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
 
 function App() {
-  const [data, setData] = useState<Post>([]);
-
-  const getData = () => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => setData(json));
-  };
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  return (
-    <div className="App">
-      <h2>CIBUS</h2>
-      {data && data.map((item: any) => <h2 key={item.id}>{item.title}</h2>)}
-    </div>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    )
   );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
